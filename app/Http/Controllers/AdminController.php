a5b77f79b59fbef79de5f3c43932dae76a9e08aa
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2023-10-14 13:44:02 +0500

    admin profile & image update

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard() {
+        return view("admin.index");
+    }
+
+    public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login/login');
+    }
+
+    public function AdminLogin() {
+        return view('admin.admin_login');
+    }
+
+}
+
+
